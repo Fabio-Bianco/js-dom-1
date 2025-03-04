5334c2b0af631b75da6b304b510d666ac15fd74c
Fabio-Bianco <biancobfabio@gmail.com>
2025-03-04 17:17:42 +0100

    feat: created const lamp & const switchButton

--- main.js
+++ main.js
@@ -1,2 +1,5 @@
+// Creazione di due variabili che contengono i riferimenti agli elementi HTML con id 'lamp' e 'switchButton'selezionati tramite il metodo document.getElementById() (selettore per id); 
+ 
+const lamp = document.getElementById('lamp');
+const switchButton = document.getElementById('switchButton');
 
-console.log('prova');
