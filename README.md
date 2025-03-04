40d15d9c4e070b845beae219cb4c0576601e9c44
Fabio-Bianco <biancobfabio@gmail.com>
2025-03-04 20:40:03 +0100

    Resolved EX with method.includes ()

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Primi passi col DOM
 
+Creazione di una pagina html contenente una lampadina ed un bottone di accensioe e spegniemto;
+
--- main.js
+++ main.js
@@ -4,22 +4,34 @@ const lamp = document.getElementById('lamp');
 const switchButton = document.getElementById('switchButton');
 
 
-// Variabile booleana on(off) per la lampadina
-let lampOn = false; // false = spenta, true = accesa
+// // Variabile booleana on(off) per la lampadina
+// let lampOn = false; // false = spenta, true = accesa
 
-// Aggiungiamo un event listener al bottone
-switchButton.addEventListener('click', function() {
-  // Invertiamo lo stato
-  lampOn = !lampOn;
+// // Aggiungiamo un event listener al bottone
+// switchButton.addEventListener('click', function() {
+//   // Invertiamo lo stato
+//   lampOn = !lampOn;
 
-    // Istruzioni condizionali per accensione/spegnimento della lampadina;
-    if (lampOn) {
-        lamp.src = 'img/yellow_lamp.png';
-        switchButton.textContent = 'On';
-      } else {
-        lamp.src = 'img/white_lamp.png';
-        switchButton.textContent = 'Off';
-      }
-    });
+//     // Istruzioni condizionali per accensione/spegnimento della lampadina;
+//     if (lampOn) {
+//         lamp.src = 'img/yellow_lamp.png';
+//         switchButton.textContent = 'On';
+//       } else {
+//         lamp.src = 'img/white_lamp.png';
+//         switchButton.textContent = 'Off';
+//       }
+//     });
 
 // RISULTATO ALTERNATIVO UTILIZZANDO include();
+
+// Button event listener
+switchButton.addEventListener('click', function() {
+  // Istruzioni condizionali per accensione/spegnimento della lampadina;
+  if (lamp.src.includes('yellow_lamp.png')) {
+    lamp.src = 'img/white_lamp.png';
+    switchButton.textContent = 'Off';
+  } else {
+    lamp.src = 'img/yellow_lamp.png';
+    switchButton.textContent = 'On';
+  }
+});
